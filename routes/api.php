2eeb86461164e0dc636c466160184fc786c5db5d
unknown <sigmaligma@skibidi.com>
2024-09-24 14:21:38 +0300

    added the filter and gold memebers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+    //return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::apiResource('customers', CustomerController::class);
+
+//Route::get('/', function(){
+    //return'API';
+//});
