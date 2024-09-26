39174fe597df4df6342a1f550094dc7714007974
unknown <sigmaligma@skibidi.com>
2024-09-26 14:54:18 +0300

    auth progress

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,14 +7,16 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request){
+
+       
         $fields = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
-            'password' => 'rquired|confirmed'
+            'password' => 'required|confirmed'
         ]);
-
+      
         $user = User::create($fields);
-
+        
         $token = $user->createToken($request->name);
 
         return [
